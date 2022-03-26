bf54f57f588c6eac0b6fd49714aacd2d2e1681c2
Luciano Martins Fagundes <lmartinsfagundes@gmail.com>
2022-03-26 00:24:59 -0300

    index.update commit
    v.0.3.0

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
 <?php
-
+$idPM = rand(1, 15);
+$idPF = rand(1, 4);
+$idM = rand(0, 19);
+$idF = rand(0, 19);
 #Buscando dados na API
 
     #Personagem Masculina
-    $characterM = file_get_contents("https://rickandmortyapi.com/api/character/122");
-
+    $characterM = file_get_contents("https://rickandmortyapi.com/api/character/?page=$idPM&status=alive&gender=male");
+#var_dump($characterM);
     #Personagem Feminina
-    $characterF = file_get_contents("https://rickandmortyapi.com/api/character/179");
+    $characterF = file_get_contents("https://rickandmortyapi.com/api/character/?page=$idPF&status=alive&gender=female");
    
 #JSON Parse
 
     #Personagem Masculina
     $parsed_characterM = json_decode($characterM, true);
-    $pre_parsed_characterMepisode = file_get_contents($parsed_characterM["episode"][0]);
+    $pre_parsed_characterMepisode = file_get_contents($parsed_characterM["results"][$idM]["episode"][0]);
     $parsed_characterMepisode = json_decode($pre_parsed_characterMepisode, true);
 
 
     #Personagem Feminina
     $parsed_characterF = json_decode($characterF, true);
-    $pre_parsed_characterFepisode = file_get_contents($parsed_characterF["episode"][2]);
+    $pre_parsed_characterFepisode = file_get_contents($parsed_characterF["results"][$idF]["episode"][0]);
     $parsed_characterFepisode = json_decode($pre_parsed_characterFepisode, true);
 
 ?>
@@ -65,12 +68,12 @@
     <div class="linha">
         <div class="charM">
             <div class="card">
-                <h1><b><?=$parsed_characterM['name'];?></b></h1>
-                <img src="<?=$parsed_characterM['image']?>" alt="Avatar" style="">
+                <h1><b><?=$parsed_characterM['results'][$idM]['name'];?></b></h1>
+                <img src="<?=$parsed_characterM['results'][$idM]['image']?>" alt="Avatar" style="">
                     <div class="container">
-                    <p>Status: <?=$parsed_characterM['status'];?></p>
-                    <p>Gênero: <?=$parsed_characterM['gender'];?></p>
-                    <p>Planeta de Origem: <?=$parsed_characterM['origin']['name'];?></p>
+                    <p>Status: <?=$parsed_characterM['results'][$idM]['status'];?></p>
+                    <p>Gênero: <?=$parsed_characterM['results'][$idM]['gender'];?></p>
+                    <p>Planeta de Origem: <?=$parsed_characterM["results"][$idM]['origin']['name'];?></p>
                     <p>Episódio: <?=$parsed_characterMepisode['episode']. "\n - \n" . $parsed_characterMepisode['name'];?></p>
 
                     </div>
@@ -79,12 +82,12 @@
 
         <div class="charF">
             <div class="card">
-                <h1><b><?=$parsed_characterF['name'];?></b></h1>
-                <img src="<?=$parsed_characterF['image']?>" alt="Avatar" style="">
+                <h1><b><?=$parsed_characterF['results'][$idF]['name'];?></b></h1>
+                <img src="<?=$parsed_characterF['results'][$idF]['image']?>" alt="Avatar" style="">
                     <div class="container">
-                    <p>Status: <?=$parsed_characterF['status'];?></p>
-                    <p>Gênero: <?=$parsed_characterF['gender'];?></p>
-                    <p>Planeta de Origem: <?=$parsed_characterF['origin']['name'];?></p>
+                    <p>Status: <?=$parsed_characterF['results'][$idF]['status'];?></p>
+                    <p>Gênero: <?=$parsed_characterF['results'][$idF]['gender'];?></p>
+                    <p>Planeta de Origem: <?=$parsed_characterF['results'][$idF]['origin']['name'];?></p>
                     <p>Episódio: <?=$parsed_characterFepisode['episode']. "\n - \n" . $parsed_characterFepisode['name'];?></p>
 
                     </div>
@@ -92,6 +95,5 @@
         </div>
     </div>
 
-    <center><footer> DevbyLuxX©2022</footer></center>
+    <center><footer> @MEB@v.0.3.0 | DevbyLuxX©2022</footer></center>
 </html>
-
